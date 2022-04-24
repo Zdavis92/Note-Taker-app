37f7bc6eb08516d471529a7d62c624d909cce17e
Zach Davis <zacharydavis92@gmail.com>
2022-04-24 09:46:12 -0500

    added get route for db

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const { notes } = require('../db/db.json');
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
@@ -9,4 +10,7 @@ router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
+router.get('/api/notes', (req, res) => {
+    res.json(notes);
+})
 module.exports = router;
